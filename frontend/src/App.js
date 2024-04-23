4dea92f77c57133c3c4d2256756c2c533b56f18e
elvischiqui <elvisrchiqui@gmail.com>
2024-04-23 19:24:06 -0400

    Implemented login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,73 +1,24 @@
-import "./App.css";
+import {Routes, Route} from 'react-router-dom'
 import io from "socket.io-client";
 import { useEffect, useState } from "react";
 import CryptoJS from 'crypto-js';
-
+import Login from "./components/Login"
+import Signup from "./components/Signup"
+import Chat from "./components/Chat"
 
 const socket = io.connect("http://localhost:3001");
 
 function App() {
 
 
-  //Room State
-  const [room, setRoom] = useState("");
-
-  // Messages States
-  const [message, setMessage] = useState("");
-  const [messageReceived, setMessageReceived] = useState("");
-  const secretKey = "uI2ooxtwHeI6q69PS98fx9SWVGbpQohO"
-
-  const joinRoom = () => {
-    if (room !== "") {
-      socket.emit("join_room", room);
-    }
-  };
-
-  const sendMessage = () => {
-    const encryptedMessage = CryptoJS.AES.encrypt(message, secretKey).toString()
-    socket.emit("send_message", { encryptedMessage, room });
-  };
-
-
-  useEffect(() => {
-    socket.on("receive_message", (data) => {
-      console.log("Received data:", data); // Check received data
-      if (data && data.encryptedMessage) {
-        console.log("Encrypted message:", data.encryptedMessage); // Check encrypted message
-        try {
-          const decryptedBytes = CryptoJS.AES.decrypt(data.encryptedMessage, secretKey);
-          const decryptedMessage = decryptedBytes.toString(CryptoJS.enc.Utf8);
-          console.log("Decrypted message:", decryptedMessage);
-          setMessageReceived(decryptedMessage);
-        } catch (error) {
-          console.error("Error decrypting message:", error);
-          setMessageReceived("Error decrypting message");
-        }
-      } else {
-        console.warn("Invalid data received:", data);
-        setMessageReceived("Invalid data received");
-      }
-    });
-}, [socket]);
   return (
-    <div className="App">
-      <input
-        placeholder="Room Number..."
-        onChange={(event) => {
-          setRoom(event.target.value);
-        }}
-      />
-      <button onClick={joinRoom}> Join Room</button>
-      <input
-        placeholder="Message..."
-        onChange={(event) => {
-          setMessage(event.target.value);
-        }}
-      />
-      <button onClick={sendMessage}> Send Message</button>
-      <h1> Message:</h1>
-      {messageReceived}
-    </div>
+    <div>
+      <Routes>
+        <Route path='/' element={< Login />}/>
+        <Route path='/signup' element = {<Signup/>}/>
+        <Route path='/chat' element = {<Chat/>}/>
+      </Routes>
+  </div>
   );
 }
 
---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import axios from 'axios'
+
+const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+   
+        try{
+            const res = await axios.post('http://localhost:3001/api/auth/login', {
+                username,
+                password
+            })
+
+            const user = res.data
+            console.log('Logged in user: ', user)
+          
+        } catch (error) {
+            console.error('Error logging in: ', error.message)
+        }
+       
+    };
+
+    return (
+        <div className="h-screen flex justify-center items-center bg-[var(--uncblue)]">
+            <div className="max-w-md w-full p-8 bg-gray-200 rounded-2xl shadow-md flex flex-col justify-center items-center">
+                <h2 className="mb-4">Login</h2>
+                <form onSubmit={handleSubmit} className="w-full">
+                    <div className="mb-4">
+                        <label className="block mb-2">Username</label>
+                        <input
+                            type="username"
+                            name="username" 
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            className="w-full px-3 py-2 border rounded"
+                            required
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label className="block mb-2">Password</label>
+                        <input
+                            type="password"
+                            name="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            className="w-full px-3 py-2 border rounded"
+                            required
+                        />
+                    </div>
+                    <button type="submit" className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600">
+                        Login
+                    </button>
+                </form>
+                <p className="mt-4 text-sm">Don't have an account? <a to="/signup" className="text-blue-500">Sign up</a></p>
+           
+            </div>
+        </div>
+    );
+};
+
+export default Login;
